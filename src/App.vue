85d40e68d0ad2a84cd32652dc16e26f9eaa230f2
Robert Stein <robert@blueshoe.de>
2020-10-13 16:58:38 +0200

    - add apollo + example

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import ApolloExample from './components/ApolloExample.vue';
 
 export default Vue.extend({
   name: 'App',
