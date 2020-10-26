581919abbe0fb471ac807b22120902ad21550332
Robert Stein <robert@blueshoe.de>
2020-10-26 18:50:11 +0100

    - add graphql codegen setup

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <v-app>
+    {{ organizations }}
     <v-main>
       <router-view></router-view>
     </v-main>
@@ -8,12 +9,29 @@
 
 <script lang="ts">
 import Vue from 'vue';
+import { OrganizationsQuery, TOrganizationsQueryResult } from '@/generated/graphql';
 
 export default Vue.extend({
   name: 'App',
-
-  data: () => ({
-    //
-  }),
+  apollo: {
+    organizations: {
+      query: OrganizationsQuery,
+      result: (result: TOrganizationsQueryResult) => {
+        console.log(result.organizations);
+      },
+    },
+  },
+  created() {
+    console.log(123);
+    this.$apollo.query(OrganizationsQuery).then(
+      (result: TOrganizationsQueryResult) => {
+        if (result && result.organizations) {
+          console.log(result.organizations.filter((val) => { console.log(val); return false; }));
+        } else {
+          console.log('this is shitty!');
+        }
+      },
+    );
+  },
 });
 </script>
---
+++ src/generated/graphql.ts
@@ -0,0 +1,176 @@
+/* eslint-disable */
+import gql from 'graphql-tag';
+export type Maybe<T> = T | null;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  UUID: any;
+  DateTime: any;
+  _FieldSet: any;
+};
+
+
+
+
+
+export type TQuery = {
+  __typename?: 'Query';
+  users?: Maybe<Array<Maybe<TUserNode>>>;
+  allUsers?: Maybe<Array<Maybe<TUserNode>>>;
+  organizations?: Maybe<Array<Maybe<TOrganizationNode>>>;
+};
+
+export type TUserNode = {
+  __typename?: 'UserNode';
+  firstName: Scalars['String'];
+  lastName: Scalars['String'];
+  email: Scalars['String'];
+  id: Scalars['UUID'];
+};
+
+
+export type TOrganizationNode = {
+  __typename?: 'OrganizationNode';
+  created: Scalars['DateTime'];
+  title: Scalars['String'];
+  description?: Maybe<Scalars['String']>;
+  id: Scalars['UUID'];
+};
+
+
+export type TMutation = {
+  __typename?: 'Mutation';
+  createProject?: Maybe<TProjectMutationPayload>;
+};
+
+
+export type TMutationCreateProjectArgs = {
+  input: TProjectMutationInput;
+};
+
+export type TProjectMutationInput = {
+  title: Scalars['String'];
+  description?: Maybe<Scalars['String']>;
+  specRepository: Scalars['String'];
+  specRepositoryBranch: Scalars['String'];
+  specType: Scalars['String'];
+  repoDir?: Maybe<Scalars['String']>;
+  currentCommit?: Maybe<Scalars['String']>;
+  accessUsername: Scalars['String'];
+  accessToken: Scalars['String'];
+  id?: Maybe<Scalars['ID']>;
+  clientMutationId?: Maybe<Scalars['String']>;
+};
+
+export type TProjectMutationPayload = {
+  __typename?: 'ProjectMutationPayload';
+  project?: Maybe<TProjectNode>;
+  errors?: Maybe<Array<Maybe<TErrorType>>>;
+  clientMutationId?: Maybe<Scalars['String']>;
+};
+
+export type TProjectNode = {
+  __typename?: 'ProjectNode';
+  title: Scalars['String'];
+  description?: Maybe<Scalars['String']>;
+  id: Scalars['UUID'];
+  specRepository: Scalars['String'];
+  specRepositoryBranch?: Maybe<Scalars['String']>;
+  created: Scalars['DateTime'];
+  specType: TProjectSpecType;
+  repoDir: Scalars['String'];
+  currentCommit: Scalars['String'];
+  accessUsername: Scalars['String'];
+  accessToken: Scalars['String'];
+  applications: Array<TApplicationNode>;
+};
+
+export enum TProjectSpecType {
+  Plain = 'PLAIN',
+  Helm = 'HELM'
+}
+
+export type TApplicationNode = {
+  __typename?: 'ApplicationNode';
+  title: Scalars['String'];
+  description?: Maybe<Scalars['String']>;
+  id: Scalars['UUID'];
+  project: TProjectNode;
+  dirname: Scalars['String'];
+  type: Scalars['String'];
+  namespace: Scalars['String'];
+  environments: Array<TClusterLevelNode>;
+  deployments?: Maybe<Array<Maybe<TDeploymentNode>>>;
+  clusterlevel?: Maybe<Array<Maybe<TClusterLevelNode>>>;
+};
+
+
+export type TApplicationNodeDeploymentsArgs = {
+  level?: Maybe<Scalars['String']>;
+  switchable?: Maybe<Scalars['Boolean']>;
+};
+
+
+export type TApplicationNodeClusterlevelArgs = {
+  level?: Maybe<Scalars['String']>;
+};
+
+export type TClusterLevelNode = {
+  __typename?: 'ClusterLevelNode';
+  title: Scalars['String'];
+  description?: Maybe<Scalars['String']>;
+  id: Scalars['UUID'];
+  application: TApplicationNode;
+  type: TClusterLevelType;
+  valuesPath: Scalars['String'];
+  deployments: Array<TDeploymentNode>;
+  specsUrl?: Maybe<Scalars['String']>;
+};
+
+export enum TClusterLevelType {
+  Local = 'LOCAL',
+  Remote = 'REMOTE'
+}
+
+export type TDeploymentNode = {
+  __typename?: 'DeploymentNode';
+  title: Scalars['String'];
+  description?: Maybe<Scalars['String']>;
+  id: Scalars['UUID'];
+  ports: Scalars['String'];
+  clusterlevel: TClusterLevelNode;
+  isSwitchable: Scalars['Boolean'];
+};
+
+export type TErrorType = {
+  __typename?: 'ErrorType';
+  field: Scalars['String'];
+  messages: Array<Scalars['String']>;
+};
+
+
+export type TOrganizationsQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type TOrganizationsQueryResult = (
+  { __typename?: 'Query' }
+  & { organizations?: Maybe<Array<Maybe<(
+    { __typename?: 'OrganizationNode' }
+    & Pick<TOrganizationNode, 'title' | 'description'>
+  )>>> }
+);
+
+
+export const OrganizationsQuery = gql`
+    query OrganizationsQuery {
+  organizations {
+    title
+    description
+  }
+}
+    `;
