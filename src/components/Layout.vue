692e4ffb0be6f384671c5c844c11f3fa39efd082
Robert Stein <robert@blueshoe.de>
2021-02-16 23:22:13 +0100

    fix: change organization query

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -70,7 +70,7 @@
           <div class="organization-dropdown--notch"></div>
           <v-list>
             <v-list-item
-                v-for="organization in organizations.results"
+                v-for="organization in allOrganizations.results"
                 :key="organization.id"
                 link
                 :ripple="false"
@@ -228,7 +228,7 @@ import { OrganizationsQuery, TOrganizationsQueryResult } from '@/generated/graph
 
 @Component({
   apollo: {
-    organizations: {
+    allOrganizations: {
       query: OrganizationsQuery,
       result: (result: TOrganizationsQueryResult) => {
         console.log(result.results);
--- src/generated/graphql.ts
+++ src/generated/graphql.ts
@@ -170,6 +170,7 @@ export const OrganizationsQuery = gql`
     query OrganizationsQuery {
   allOrganizations {
     results {
+        id
         title
         description
     }
