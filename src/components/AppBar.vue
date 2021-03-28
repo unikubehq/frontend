285fc09257099c57cedefb8803093cd8249a36d4
Robert Stein <robert@blueshoe.de>
2021-03-28 12:35:57 +0200

    refactor: spacing

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -4,37 +4,17 @@
       {{ currentRoute }}
     </v-toolbar-title>
     <div class="d-flex flex-row justify-space-around align-center" style="width: 350px;">
-      <v-menu
-        v-model="notificationsMenu"
-        offset-y
-        nudge-bottom="5"
-        nudge-left="290"
-      >
+      <v-menu v-model="notificationsMenu" offset-y nudge-bottom="5" nudge-left="290">
         <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            v-bind="attrs"
-            v-on="on"
-            text
-          >
-            <v-badge
-              :content="notifications"
-              :value="notifications"
-              color="#ff5e5b"
-              overlap
-            >
-              <v-icon
-                width="40"
-                height="40"
-                @click="increment"
-              >
+          <v-btn v-bind="attrs" v-on="on" text>
+            <v-badge :content="notifications" :value="notifications" color="#ff5e5b" overlap>
+              <v-icon width="40" height="40" @click="increment">
                 $vuetify.icons.notificationBlue
               </v-icon>
             </v-badge>
           </v-btn>
         </template>
-        <v-card
-          width="340"
-        >
+        <v-card width="340">
           <v-card-title>
             Notifications
           </v-card-title>
@@ -47,47 +27,37 @@
 
       <v-divider vertical class="ml-3"></v-divider>
       <div>
-      <v-menu
-        v-model="menu"
-        offset-y
-        nudge-bottom="5"
-      >
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            color="343d6f"
-            v-bind="attrs"
-            v-on="on"
-            text
-          >
-            <v-avatar class="mr-3">
-              <img src="https://randomuser.me/api/portraits/women/81.jpg">
-            </v-avatar>
-            <div class="d-flex flex-column text-left">
-              <h3 class="mb-0">{{ username }}</h3>
-              <p class="mb-0">Administrator</p>
-            </div>
-            <v-icon class="ml-4" small>$vuetify.icons.dropdown</v-icon>
-          </v-btn>
-
-        </template>
-          <v-card>
-            <v-list>
-              <v-list-item>
-                <v-list-item-icon>
-                  <v-icon>$vuetify.icons.accountBlue</v-icon>
-                </v-list-item-icon>
-                Account Settings
-              </v-list-item>
-              <v-list-item>
-                <v-list-item-icon>
-                  <v-icon>$vuetify.icons.logout</v-icon>
-                </v-list-item-icon>
-                Logout
-              </v-list-item>
-            </v-list>
-          </v-card>
-      </v-menu>
-    </div>
+        <v-menu v-model="menu" offset-y nudge-bottom="5">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn color="343d6f" v-bind="attrs" v-on="on" text>
+              <v-avatar class="mr-3">
+                <img src="https://randomuser.me/api/portraits/women/81.jpg">
+              </v-avatar>
+              <div class="d-flex flex-column text-left">
+                <h3 class="mb-0">{{ username }}</h3>
+                <p class="mb-0">Administrator</p>
+              </div>
+              <v-icon class="ml-4" small>$vuetify.icons.dropdown</v-icon>
+            </v-btn>
+          </template>
+            <v-card>
+              <v-list>
+                <v-list-item>
+                  <v-list-item-icon>
+                    <v-icon>$vuetify.icons.accountBlue</v-icon>
+                  </v-list-item-icon>
+                  Account Settings
+                </v-list-item>
+                <v-list-item>
+                  <v-list-item-icon>
+                    <v-icon>$vuetify.icons.logout</v-icon>
+                  </v-list-item-icon>
+                  Logout
+                </v-list-item>
+              </v-list>
+            </v-card>
+        </v-menu>
+      </div>
     </div>
   </v-app-bar>
 </template>
