feeab44eb5f2eab9f8eb0d1554d54ccd85f30291
Robert Stein <robert@blueshoe.de>
2021-04-27 21:02:33 +0200

    fix: force update token when new project is created

--- src/views/Projects/ProjectForm.vue
+++ src/views/Projects/ProjectForm.vue
@@ -262,7 +262,7 @@ export default class ProjectForm extends validationMixin {
     })
       .then((data) => {
         this.saveLoading = false;
-        this.$store.dispatch('auth/refresh', 0);
+        this.$store.dispatch('auth/refresh', -1);
         if (data.data.createUpdateProject.project) {
           if (this.editMode) {
             this.$router.go(-1);
