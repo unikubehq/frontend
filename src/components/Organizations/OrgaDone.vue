5a2554489e404bfc9c04deaea9adba3d231b4f30
Robert Stein <robert@blueshoe.de>
2021-06-15 13:50:25 +0200

    feat: add loading screen to organization creation

--- src/components/Organizations/OrgaDone.vue
+++ src/components/Organizations/OrgaDone.vue
@@ -1,35 +1,20 @@
 <template>
   <v-col cols="12" sm="8" md="3" class="text-center">
-    <v-icon size="60">$vuetify.icons.done</v-icon>
-    <h1>We're all done!</h1>
-    <p class="text--secondary">Ta-da! {{ orgaTitle }} has been created successfully.
-You can share the organization ID below with the team members you’d like to add in this project</p>
-<!--    <v-text-field-->
-<!--      outlined-->
-<!--      readonly-->
-<!--      v-model="orgaId"-->
-<!--      append-icon="$vuetify.icons.copy"-->
-<!--      @click:append="copyId"-->
-<!--    >-->
-<!--    </v-text-field>-->
-    <router-link to="/overview">Go to Dashboard</router-link>
-<!--  <v-snackbar-->
-<!--      v-model="snackbar"-->
-<!--      color="primary"-->
-<!--    >-->
-<!--      Copied to Clipboard-->
-
-<!--      <template v-slot:action="{ attrs }">-->
-<!--        <v-btn-->
-<!--          color="white"-->
-<!--          text-->
-<!--          v-bind="attrs"-->
-<!--          @click="snackbar = false"-->
-<!--        >-->
-<!--          Close-->
-<!--        </v-btn>-->
-<!--      </template>-->
-<!--    </v-snackbar>-->
+    <div v-if="rptRefreshed">
+      <v-icon size="60">$vuetify.icons.done</v-icon>
+      <h1>We're all done!</h1>
+      <p class="text--secondary">Ta-da! {{ orgaTitle }} has been created successfully.
+      You can share the organization ID below with the team members you’d like to add in
+      this project</p>
+      <router-link to="/overview">Go to Dashboard</router-link>
+    </div>
+    <div v-else>
+      <div class="text-center">
+        <div class="orga-loader"></div>
+      </div>
+      <h1>Finishing up...</h1>
+      <p class="text--secondary">Please wait while we create a new organization for you.</p>
+    </div>
   </v-col>
 </template>
 
@@ -44,8 +29,44 @@ export default class OrgaDone extends Vue {
 
   snackbar = false;
 
+  rptRefreshed = false;
+
   created(): void {
-    this.$store.dispatch('auth/refresh', -1);
+    this.$store.dispatch('auth/refresh', -1).then(() => {
+      this.rptRefreshed = true;
+    });
   }
 }
 </script>
+
+<style scoped>
+.orga-loader {
+  width: 20px;
+  height: 20px;
+  position: relative;
+  animation: loading 1s ease infinite;
+  top: 50%;
+  margin: 0 auto 0 auto;
+  margin-bottom: 80px;
+}
+
+@keyframes loading {
+  0%, 100% {
+    box-shadow: -13px 20px 0 #00DAA4, 13px 20px 0 #61DFC0, 13px 46px 0 #A2E7D6,
+    -13px 46px 0 #CAF1E7;
+  }
+  25% {
+    box-shadow: -13px 20px 0 #CAF1E7, 13px 20px 0 #00DAA4, 13px 46px 0 #61DFC0,
+    -13px 46px 0 #A2E7D6;
+  }
+  50% {
+    box-shadow: -13px 20px 0 #A2E7D6, 13px 20px 0 #CAF1E7, 13px 46px 0 #00DAA4,
+    -13px 46px 0 #61DFC0;
+  }
+  75% {
+    box-shadow: -13px 20px 0 #61DFC0, 13px 20px 0 #A2E7D6, 13px 46px 0 #CAF1E7,
+    -13px 46px 0 #00DAA4;
+  }
+}
+
+</style>
