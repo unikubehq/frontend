72009aaaf5a71682c7a2f1a24481c05184b4b152
Robert Stein <SteinRobert@users.noreply.github.com>
2021-11-30 02:05:08 +0100

    fix: path resolve

--- tests/e2e/plugins/index.js
+++ tests/e2e/plugins/index.js
@@ -5,7 +5,7 @@
 // and then use the `file:preprocessor` event
 // as explained in the cypress docs
 // https://docs.cypress.io/api/plugins/preprocessors-api.html#Examples
-
+const path = require('path');
 // /* eslint-disable import/no-extraneous-dependencies, global-require */
 const webpack = require('@cypress/webpack-preprocessor');
 // The webpack pre-processor is used to make sure all compiled files are also
@@ -18,7 +18,7 @@ module.exports = (on, config) => {
   const options = webpack.defaultOptions;
   options.webpackOptions.resolve = {
     alias: {
-      '@': '/Users/robertstein/Projects/unikube-frontend/src',
+      '@': path.resolve(__dirname, '../../../src'),
     },
     extensions: [
       '.tsx',
@@ -34,17 +34,17 @@ module.exports = (on, config) => {
     test: /\.ts$/,
     use: [
       {
-        loader: '/Users/robertstein/Projects/unikube-frontend/node_modules/cache-loader/dist/cjs.js',
+        loader: './node_modules/cache-loader/dist/cjs.js',
         options: {
-          cacheDirectory: '/Users/robertstein/Projects/unikube-frontend/node_modules/.cache/ts-loader',
+          cacheDirectory: './node_modules/.cache/ts-loader',
           cacheIdentifier: '21abf4fa',
         },
       },
       {
-        loader: '/Users/robertstein/Projects/unikube-frontend/node_modules/babel-loader/lib/index.js',
+        loader: './node_modules/babel-loader/lib/index.js',
       },
       {
-        loader: '/Users/robertstein/Projects/unikube-frontend/node_modules/ts-loader/index.js',
+        loader: './node_modules/ts-loader/index.js',
         options: {
           transpileOnly: true,
           appendTsSuffixTo: [
