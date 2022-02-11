8fb325389a2b53b73c2d82548e7f69232989244f
Robert Stein <SteinRobert@users.noreply.github.com>
2022-02-11 16:39:06 +0100

    chore(vue3): update App.vue

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -12,27 +12,19 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import Component from 'vue-class-component';
+import { defineComponent } from 'vue';
 import Navigation from '@/components/Navigation.vue';
 import AppBar from '@/components/AppBar.vue';
 import ErrorHandler from '@/components/ErrorHandler.vue';
 
-Component.registerHooks([
-  'beforeRouteEnter',
-  'beforeRouteUpdate',
-  'beforeRouteLeave',
-]);
-
-@Component({
+export default defineComponent({
+  name: 'UnikubeLayout',
   components: {
     AppNavigation: Navigation,
     AppBar,
     ErrorHandler,
   },
-})
-export default class Layout extends Vue {
-}
+});
 </script>
 
 <style scoped>
